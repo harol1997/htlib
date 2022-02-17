318ceedfe54e14bdceeb755e549e0161811a9837
Harol <harolav3@gmail.com>
2022-02-16 20:41:06 -0500

    change release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Harol Alvarado'
 author = 'Harol Alvarado'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1'
+release = '3'
 
 
 # -- General configuration ---------------------------------------------------
